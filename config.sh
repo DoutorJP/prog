55f0d0cfc5d36efab7992c9d35ea1f19072015a2
DoutorJP <jpmv14@gmail.com>
2021-05-07 16:42:24 -0300

    commit

--- config.sh
+++ config.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 echo "alias prog='./prog.sh'" > ~/.bashrc
-
+if [[! -d "$DIRECTORY" ]]
+then
+mkdir ~/Documentos/Programacao
+mkdir ~/Documentos/Programacao/C
+mkdir ~/Documentos/Programacao/PYTHON
+mkdir ~/Documentos/Programacao/JAVA
+mkdir ~/Documentos/Programacao/C++
+mkdir ~/Documentos/Programacao/SHELL
+fi
 
 
 
