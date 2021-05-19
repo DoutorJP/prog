8d464ac93f20a7b247e532336d584dc154fa0436
DoutorJP <jpmv14@gmail.com>
2021-05-19 17:22:33 -0300

    version 1.3 - compatibilidade com nano

--- prog.sh
+++ prog.sh
@@ -22,63 +22,10 @@ then
 		echo " -cpp			             C++		 "
 		echo "-p 			             PYTHON		 "
 fi
-if [[ "$2" == "-v" ]]
-then
-
-		if [[ "$1" == "-s" ]]
-		then
-				echo "shell"	
-				cd ~/Documentos/Programacao/SHELL	
-				vim $3
-		fi
-
-		if [[ "$1" == "-j" ]]
-		then
-				echo "java"	
-				cd ~/Documentos/Programacao/JAVA
-				vim $3
-		fi
-
-
-		if [[ "$1" == "-c" ]]
-		then
-				echo "c"	
-				cd ~/Documentos/Programacao/C
-				vim $3
-		fi
-
-
-		if [[ "$1" == "-cpp" ]]
-		then
-				echo "cpp"	
-				cd ~/Documentos/Programacao/C++
-				vim $3
-		fi
-
-
-		if [[ "$1" == "-p" ]]
-		then
-				echo "python"	
-				cd ~/Documentos/Programacao/PYTHON
-				vim $3
-		fi
-fi
 
 if [[ $2 == "-n"  ]] 
 then
 
-		if [[ "$1" == "-h" ]]
-		then
-				echo "Sintaxe: ./prog.sh <opcao> <nome do arquivo>"
-				echo "============================================"
-				echo " OPCAO                     LINGUAGEM"
-				echo "============================================"
-				echo " -s			SHELL SCRIPT     "
-				echo " -j			JAVA		 "
-				echo " -c 			C		 "
-				echo " -cpp			C++		 "
-				echo "-p 			PYTHON		 "
-		fi
 		if [[ "$1" == "-s" ]]
 		then
 				echo "shell"	
@@ -116,6 +63,47 @@ then
 				cd ~/Documentos/Programacao/PYTHON
 				nano $3
 		fi
-fi
 
+else
+
+
+
+		if [[ "$1" == "-s" ]]
+		then
+				echo "shell"	
+				cd ~/Documentos/Programacao/SHELL	
+				vim $3
+		fi
+
+		if [[ "$1" == "-j" ]]
+		then
+				echo "java"	
+				cd ~/Documentos/Programacao/JAVA
+				vim $3
+		fi
+
+
+		if [[ "$1" == "-c" ]]
+		then
+				echo "c"	
+				cd ~/Documentos/Programacao/C
+				vim $3
+		fi
+
+
+		if [[ "$1" == "-cpp" ]]
+		then
+				echo "cpp"	
+				cd ~/Documentos/Programacao/C++
+				vim $3
+		fi
+
+
+		if [[ "$1" == "-p" ]]
+		then
+				echo "python"	
+				cd ~/Documentos/Programacao/PYTHON
+				vim $3
+		fi
+fi
 
